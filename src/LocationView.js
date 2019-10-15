a18e1c52385be1939a148068133406b589bf3ea8
gpaoloni <paolonigianfranco@gmail.com>
2019-10-15 14:41:11 -0300

    Fixed geolocation services

--- src/LocationView.js
+++ src/LocationView.js
@@ -6,7 +6,7 @@ import Entypo from 'react-native-vector-icons/Entypo';
 import axios from 'axios';
 import Events from 'react-native-simple-events';
 import MapView from 'react-native-maps';
-
+import Geolocation from 'react-native-geolocation-service'
 import AutoCompleteInput from './AutoCompleteInput';
 
 const PLACE_DETAIL_URL = 'https://maps.googleapis.com/maps/api/place/details/json';
@@ -106,7 +106,7 @@ export default class LocationView extends React.Component {
   };
 
   _getCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition(position => {
+    Geolocation.getCurrentPosition(position => {
       let location = (({ latitude, longitude }) => ({ latitude, longitude }))(position.coords);
       this._setRegion(location);
     });
